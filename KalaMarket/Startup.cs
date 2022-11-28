57cba0534411144a726b4e077d328de9664a78dc
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-11-28 22:06:02 +0330

    Add Area Admin

--- KalaMarket/Startup.cs
+++ KalaMarket/Startup.cs
@@ -47,6 +47,10 @@ namespace KalaMarket
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                
+                endpoints.MapControllerRoute(
+                    name: "areas",
+                    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
