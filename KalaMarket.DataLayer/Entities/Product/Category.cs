1f9b11c79d986d43b87c8fa1a26ea7e14f879cd9
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-12-04 19:47:50 +0330

    ProductGarranty Model

--- KalaMarket.DataLayer/Entities/Product/Category.cs
+++ KalaMarket.DataLayer/Entities/Product/Category.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace KalaMarket.DataLayer.Entities.Product
 {
@@ -12,16 +14,30 @@ namespace KalaMarket.DataLayer.Entities.Product
         [Key] public int CategoryId { get; set; }
 
         [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
-        [Display(Name = "عنوان دسته بندی")]
+        [Display(Name = "عنوان دسته بندی فارسی")]
         [MaxLength(100, ErrorMessage = "* وارد کردن بیش از {0} کاراکتر مجاز نیست")]
         [MinLength(3, ErrorMessage = "* وارد کردن کمتر از {0} کاراکتر مجاز نیست")]
-        public string CategoryName { get; set; }
+        public string categoriFaTitle { get; set; }
+
+
+        [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
+        [Display(Name = "عنوان دسته بندی انگلیسی")]
+        [MaxLength(100, ErrorMessage = "* وارد کردن بیش از {0} کاراکتر مجاز نیست")]
+        [MinLength(3, ErrorMessage = "* وارد کردن کمتر از {0} کاراکتر مجاز نیست")]
+        public string categoriEnTitle { get; set; }
+
+        public int SubCategory { get; set; }
+
+        public bool IsDeleted { get; set; }
 
         #region Relation
 
         public List<Product> Products { get; set; }
 
         public List<PropertyNameToCategory> PropertyNameToCategories { get; set; }
+
+        [ForeignKey("SubCategory")]
+        public Category Category1 { get; set; }
 
         #endregion
     }
---
+++ KalaMarket.DataLayer/Entities/Product/ProductGarranty.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaMarket.DataLayer.Entities.Product
+{
+    public class ProductGarranty
+    {
+        [Key] public int ProductGarrantyId { get; set; }
+
+        [MinLength(5, ErrorMessage = "* وارد کردن کمتر از {0} کاراکتر مجاز نیست")]
+        [MaxLength(150, ErrorMessage = "* وارد کردن بیش از {0} کاراکتر مجاز نیست")]
+        [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
+        [Display(Name = "نام گارانتی")]
+        public string ProductGarrantyName { get; set; }
+    }
+}
