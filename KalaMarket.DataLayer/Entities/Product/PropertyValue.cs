3b3e16018ca9f2b5038e983027a03f72c77236ec
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-12-04 19:33:05 +0330

    PropertyValue Model

---
+++ KalaMarket.DataLayer/Entities/Product/PropertyValue.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaMarket.DataLayer.Entities.Product
+{
+    public class PropertyValue
+    {
+        [Key] public int PropertyValueId { get; set; }
+
+        [MinLength(5, ErrorMessage = "* وارد کردن کمتر از {0} کاراکتر مجاز نیست")]
+        [MaxLength(100, ErrorMessage = "* وارد کردن بیش از {0} کاراکتر مجاز نیست")]
+        [Display(Name = "مقدار خصوصیت")]
+        [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
+        public string PropertyValueText { get; set; }
+
+        public int ProductId { get; set; }
+
+        #region Relation
+
+        [ForeignKey("ProductId")] public Product Product { get; set; }
+
+        #endregion
+    }
+}
