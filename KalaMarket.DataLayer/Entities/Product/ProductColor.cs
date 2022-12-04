8ff34ab1c7b12628ecdd74de61204ce3f2cdf4e7
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-12-04 19:41:03 +0330

    ProductColor Model

---
+++ KalaMarket.DataLayer/Entities/Product/ProductColor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaMarket.DataLayer.Entities.Product
+{
+    public class ProductColor
+    {
+        [Key] public int ProductColorId { get; set; }
+
+        [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
+        [Display(Name = "نام رنگ")]
+        [MaxLength(50, ErrorMessage = "* وارد کردن بیش از {0} کاراکتر مجاز نیست")]
+        [MinLength(3, ErrorMessage = "* وارد کردن کمتر از {0} کاراکتر مجاز نیست")]
+        public string ProductColorName { get; set; }
+
+        [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
+        [Display(Name = "کد رنگ")]
+        [MaxLength(20, ErrorMessage = "* وارد کردن بیش از {0} کاراکتر مجاز نیست")]
+        public string ProductColorCode { get; set; }
+    }
+}
