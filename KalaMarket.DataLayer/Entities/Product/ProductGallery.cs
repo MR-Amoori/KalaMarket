dc2152c121ff1f4e65794f2b9f5aa9cae68014fb
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-12-04 19:38:12 +0330

    ProductGallery Model

---
+++ KalaMarket.DataLayer/Entities/Product/ProductGallery.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace KalaMarket.DataLayer.Entities.Product
+{
+    public class ProductGallery
+    {
+        [Key] public int ProductGalleryId { get; set; }
+
+        [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
+        [Display(Name = "تصویر محصول")]
+        public string ImageName { get; set; }
+
+        [MinLength(5, ErrorMessage = "* وارد کردن کمتر از {0} کاراکتر مجاز نیست")]
+        [MaxLength(50, ErrorMessage = "* وارد کردن بیش از {0} کاراکتر مجاز نیست")]
+        [Display(Name = "آلت تصویر")]
+        [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
+        public string ImageAlt { get; set; }
+
+        public int ProductId { get; set; }
+
+        #region Relation
+
+        [ForeignKey("ProductId")] public Product Product { get; set; }
+
+        #endregion
+    }
+}
