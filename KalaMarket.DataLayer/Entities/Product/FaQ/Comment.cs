964ae7ae4eaa6e86a0cbe92bc25549f4e088a31b
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-12-04 19:57:52 +0330

    Comment & User Model

---
+++ KalaMarket.DataLayer/Entities/Product/FaQ/Comment.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace KalaMarket.DataLayer.Entities.Product.FaQ
+{
+    public class Comment
+    {
+        [Key] public int CommentId { get; set; }
+
+        [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
+        [Display(Name = "عنوان نظر")]
+        [MaxLength(200, ErrorMessage = "* وارد کردن بیش از {0} کاراکتر مجاز نیست")]
+        [MinLength(4, ErrorMessage = "* وارد کردن کمتر از {0} کاراکتر مجاز نیست")]
+        public string CommentTitle { get; set; }
+
+        [MinLength(10, ErrorMessage = "* وارد کردن کمتر از {0} کاراکتر مجاز نیست")]
+        [MaxLength(500, ErrorMessage = "* وارد کردن بیش از {0} کاراکتر مجاز نیست")]
+        [Display(Name = "توضیحات نظر")]
+        [Required(ErrorMessage = "* وارد کردن {0} اجباری است")]
+        public string CommentDescription { get; set; }
+
+        public int CommentLike { get; set; }
+        public int CommentDislike { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        public bool IsActive { get; set; }
+        public byte ReComment { get; set; }
+
+        public int ProductId { get; set; }
+        public int UserId { get; set; }
+
+        #region Relation
+
+        [ForeignKey("ProductId")] public Product Product { get; set; }
+
+        [ForeignKey("UserId")] public User User { get; set; }
+
+        #endregion
+    }
+}
---
+++ KalaMarket.DataLayer/Entities/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalaMarket.DataLayer.Entities
+{
+   public class User
+    {
+        [Key]
+        public int UserId { get; set; }
+        public string UserAccount { get; set; }
+        public string UserName { get; set; }
+        public string UserFamily { get; set; }
+        public string Password { get; set; }
+        public string Phone { get; set; }
+        public DateTime RegisterDate { get; set; }
+        public string Email { get; set; }
+    }
+}
