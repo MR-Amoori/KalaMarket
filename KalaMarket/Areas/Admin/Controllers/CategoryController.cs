3503de41a83e7f28d506842ee9f97fa5ecbb8a64
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-12-08 04:43:28 +0330

    Add Category

---
+++ KalaMarket/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using KalaMarket.Core.Service.Interface;
+using KalaMarket.DataLayer.Entities.Product;
+
+namespace KalaMarket.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_categoryService.ShowAllCategory());
+        }
+
+        [HttpGet]
+        public IActionResult AddCategory()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddCategory(Category category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_categoryService.AddCategory(category) != 0)
+                {
+                    TempData["Result"] = "added";
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(category);
+        }
+
+
+    }
+}
