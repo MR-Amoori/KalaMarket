59f4e3e1754ab7aa7a121c01ff5f8ad9e680504b
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-12-10 18:37:58 +0330

    CRUD Garranty

--- KalaMarket/Areas/Admin/Controllers/GarrantyController.cs
+++ KalaMarket/Areas/Admin/Controllers/GarrantyController.cs
@@ -24,23 +24,86 @@ namespace KalaMarket.Areas.Admin.Controllers
             return View(_garrantyService.ShowAllGarranties());
         }
 
+        #region AddGarranty
+
+        //[HttpGet]
+        //public IActionResult AddGarranty()
+        //{
+        //    return View();
+        //}
+
+        //[HttpPost]
+        //public IActionResult AddGarranty(ProductGarranty garranty)
+        //{
+        //    if (!ModelState.IsValid) return View(garranty);
+
+        //    if (_garrantyService.AddGarranty(garranty) > 0)
+        //    {
+        //        return RedirectToAction(nameof(Index));
+        //    }
+
+        //    return View(garranty);
+        //}
+
+        #endregion
+
         [HttpGet]
         public IActionResult AddGarranty()
         {
-            return View();
+            return PartialView("");
         }
 
-        [HttpPost]
         public IActionResult AddGarranty(ProductGarranty garranty)
         {
-            if (!ModelState.IsValid) return View(garranty);
-
-            if (_garrantyService.AddGarranty(garranty) > 0)
+            if (!ModelState.IsValid)
             {
                 return RedirectToAction(nameof(Index));
             }
 
-            return View(garranty);
+            int garrantyId = _garrantyService.AddGarranty(garranty);
+            int sendJson = garrantyId > 0 ? 1 : 0;
+            return Json(sendJson);
+        }
+
+
+        [HttpGet]
+        public IActionResult UpdateGarranty(int garrantyId)
+        {
+            var garranty = _garrantyService.FindGarrantyBy(garrantyId);
+            return PartialView("",garranty);
+        }
+
+        public IActionResult UpdateGarranty(ProductGarranty garranty)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            bool result = _garrantyService.UpdateGarranty(garranty);
+            int sendJson = result == true ? 2 : 0;
+            return Json(sendJson);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult DeleteGarranty(int garrantyId)
+        {
+            var garranty = _garrantyService.FindGarrantyBy(garrantyId);
+            return PartialView("", garranty);
+        }
+
+        public IActionResult DeleteGarranty(ProductGarranty garranty)
+        {
+            if (!ModelState.IsValid)
+            {
+                return RedirectToAction(nameof(Index));
+            }
+
+            bool result = _garrantyService.DeleteGarranty(garranty.ProductGarrantyId);
+            int sendJson = result == true ? 3 : 0;
+            return Json(sendJson);
         }
     }
 }
