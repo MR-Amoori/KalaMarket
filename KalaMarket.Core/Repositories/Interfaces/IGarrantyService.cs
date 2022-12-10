86e668b7241eed70f814736c0a1b678c4d9d87e1
Mohamad Reza Amoori <mohamad.reza.amoori99@gmail.com>
2022-12-10 16:09:04 +0330

    Garranty Repository

---
+++ KalaMarket.Core/Repositories/Interfaces/IGarrantyService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using KalaMarket.DataLayer.Entities.Product;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace KalaMarket.Core.Service.Interface
+{
+    public interface IGarrantyService
+    {
+        List<ProductGarranty> ShowAllGarranties();
+        ProductGarranty FindGarrantyBy(int id);
+        bool UpdateGarranty(ProductGarranty garranty);
+        bool DeleteGarranty(ProductGarranty garranty);
+        bool ExistGarrante(string garrantiName, int garrantyId);
+        int AddGarranty(ProductGarranty garranty);
+        void Save();
+    }
+}
---
+++ KalaMarket.Core/Repositories/Services/GarrantyService.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using KalaMarket.Core.Service.Interface;
+using KalaMarket.DataLayer.Context;
+using KalaMarket.DataLayer.Entities.Product;
+
+namespace KalaMarket.Core.Repositories.Services
+{
+    public class GarrantyService : IGarrantyService
+    {
+        private readonly KalaMarketContext _context;
+
+        public GarrantyService(KalaMarketContext context)
+        {
+            _context = context;
+        }
+
+        public List<ProductGarranty> ShowAllGarranties()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ProductGarranty FindGarrantyBy(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool UpdateGarranty(ProductGarranty garranty)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool DeleteGarranty(ProductGarranty garranty)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool ExistGarrante(string garrantiName, int garrantyId)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int AddGarranty(ProductGarranty garranty)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void Save()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
